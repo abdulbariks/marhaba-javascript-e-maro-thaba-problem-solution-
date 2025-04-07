d32ebff4f089dbb394d2e50c0eafac6610bbc087
Abdul Barik <abdulbarik1997m@gmail.com>
2025-04-08 02:14:53 +0600

    comparison 1st practice

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Marhaba JavaScript e Maro Thaba Problem Solution");
+// Practice 1:
+const amiKheyechi = 7;
+const vaiKheyeche = 5;
+console.log(amiKheyechi > vaiKheyeche);
+// Practice 2:
+const amarClassStudent = 45;
+const paserClassStudent = 50;
+console.log(amarClassStudent < paserClassStudent);
+// Practice 3:
+const amarNumber = 75;
+const friendNumber = 75;
+console.log(amarNumber == friendNumber);
+// Practice 4:
+const amarTarget = 100;
+const amiJomaisi = 95;
+console.log(amarTarget <= amiJomaisi);
+
+// Practice 5:
+console.log(10 != 7);
+
+// Practice 6:
+console.log(20 <= 15);
+
+// Practice 7:
+const amiNumberPaisi = 45;
+const amarBondhuNumberPaise = 95;
+console.log(amiNumberPaisi > amarBondhuNumberPaise);
