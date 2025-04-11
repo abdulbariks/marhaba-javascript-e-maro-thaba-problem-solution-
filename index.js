284862846e3b31b1e3f75f37eb25c09dde220921
Abdul Barik <abdulbarik1997m@gmail.com>
2025-04-12 02:42:18 +0600

    condition Practice-1 & Practice-2 done

--- index.js
+++ index.js
@@ -1,1 +1,115 @@
-console.log("Marhaba JavaScript e Maro Thaba Problem Solution");
+// Practice:1
+// problem 1:
+const time = 6;
+if (time < 6) {
+  console.log("nasta khayabe");
+} else {
+  console.log("jharur bari dibe");
+}
+// problem 2:
+const isLogin = true;
+if (isLogin == true) {
+  console.log("Welcome");
+} else {
+  console.log("Get Lost");
+}
+// problem 3:
+const run = 5;
+if (run == 5) {
+  console.log("cocolate kpabo");
+} else {
+  console.log("vuri barbe");
+}
+// problem 4:
+const mark = 85;
+if (mark > 80) {
+  console.log("bike pabo");
+} else {
+  console.log("bike pabo na");
+}
+// problem 5:
+const movetime = 6;
+if (movetime < 6) {
+  console.log("movie dekhbo");
+} else {
+  console.log("nake tel diya gumabo");
+}
+// problem 6:
+const tamparature = 27;
+if (tamparature >= 30) {
+  console.log("AC chalabo");
+} else {
+  console.log("kombol muri diya ghumabo");
+}
+
+// Practice:2
+// problem 1:
+const age = 17;
+const hight = 60;
+if (age > 18 && hight > 60) {
+  console.log("gari thelbe");
+} else {
+  console.log("garit bose thakbe");
+}
+// problem 2:
+const math = 90;
+const english = 86;
+if (math > 80 && english > 85) {
+  console.log("vorti hoyar juggo");
+} else {
+  console.log("biya diye dao");
+}
+// problem 3:
+const gpa = 5;
+const income = 9000;
+if (gpa == 5 && income < 10000) {
+  console.log("scolarship pabe");
+} else {
+  console.log("taka diye porte hobe");
+}
+// problem 4:
+const jobholderage = 29;
+const experience = 3;
+if (jobholderage < 30 && experience > 2) {
+  console.log("job exam dite parbe");
+} else {
+  console.log("job exam dite parbe na");
+}
+// problem 5:
+const ege = 10;
+const murgi = true;
+if (ege > 12 || murgi == true) {
+  console.log("korma ranna hobe");
+} else {
+  console.log("par ruti kola khabo");
+}
+// problem 6:
+const tapmatra = 100;
+const kashi = false;
+if (tapmatra > 100 || kashi == true) {
+  console.log("doctor er kase jabe");
+} else {
+  console.log("katha mure shuye thakbe");
+}
+// problem 7:
+const attendance = 88;
+const homeWork = true;
+if (attendance > 80 && homeWork == true) {
+  console.log("exam dite parbe");
+} else {
+  console.log("auto fail");
+}
+// problem 8:
+const currentAse = false;
+const mobilechargeAse = true;
+if (currentAse == false && mobilechargeAse == false) {
+  console.log("porte bosbo");
+} else {
+  console.log("video game khelbo");
+}
+// problem 9:
+const shirtprice = 11000;
+const cupone = true;
+if (shirtprice > 1000 && cupone == true) {
+  console.log("20% discount pabe");
+}
