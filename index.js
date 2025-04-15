6787bb95a0f9b41216a0deecf31d9f2bebd43109
Abdul Barik <abdulbarik1997m@gmail.com>
2025-04-15 18:10:42 +0600

    variable all practice done

--- index.js
+++ index.js
@@ -1,4 +1,133 @@
-const totalIncome = 100;
-const totalCost = 40;
-const remaining = totalIncome - totalCost;
-console.log(remaining);
+// practice 1:
+// Problem 1:
+const myBirthday = 1197;
+// Problem 2:
+const totalVaiBon = 8;
+// Problem 3:
+const students = 75;
+// Problem 4:
+const marks = 12;
+// Problem 5:
+const rooms = 6;
+// Problem 6:
+const hours = 2;
+// Problem 7:
+const total = 11;
+
+// practice 2:
+// Problem 1:
+const age = 28;
+console.log(age);
+// Problem 2:
+const booksToRead = 10;
+console.log(booksToRead);
+// Problem 3:
+const wakeUpTime = 12;
+console.log(wakeUpTime);
+// Problem 4:
+const wastedTime = 3;
+console.log(wastedTime);
+// Problem 5:
+const borrow = 5000;
+console.log(borrow);
+// Problem 6:
+const goals = 4;
+console.log(goals);
+// Problem 7:
+const pillow = 2;
+console.log(pillow);
+
+// practice 3:
+// Problem 1:
+const name = "Abdul Barik";
+console.log(name);
+// Problem 2:
+const address = "Tista , Lalmonirhat";
+console.log(address);
+// Problem 3:
+const varsity = "UCASM";
+console.log(varsity);
+// Problem 4:
+const fan = "preity zinta";
+console.log(fan);
+// Problem 5:
+const movieOfTheYear = "laapataa ladies";
+console.log(movieOfTheYear);
+// Problem 6:
+const hobby = "travelling";
+console.log(hobby);
+// Problem 7:
+const color = "white";
+console.log(color);
+
+// practice 4:
+// Problem 1:
+const happy = false;
+console.log(happy);
+// Problem 2:
+const isMessage = true;
+console.log(isMessage);
+// Problem 3:
+const celebrating = true;
+console.log(celebrating);
+// Problem 4:
+const isHappy = true;
+const hasMoney = false;
+const isHungry = true;
+console.log(isHappy, hasMoney, isHungry);
+// Problem 5:
+const isStudyDone = false;
+console.log(isStudyDone);
+// Problem 6:
+const isPhoneCharged = false;
+console.log(isPhoneCharged);
+// Problem 7:
+const isDinnerDone = true;
+console.log(isDinnerDone);
+
+// practice 5:
+// Problem 1:
+const income = 100;
+const expense = 40;
+const saved = income - expense;
+console.log(saved);
+// Problem 2:
+const totalPencile = 10;
+const pardayUsed = 2;
+const totalUsedDay = 10 / 2;
+console.log(totalUsedDay);
+// Problem 3:
+const hasTaka = 60;
+const totalUsingCount = 15;
+const tatalUsed = hasTaka / totalUsingCount;
+const obosistoTaka = hasTaka % totalUsingCount;
+console.log(tatalUsed, obosistoTaka);
+// Problem 4:
+const fristString = "Hello";
+const secoundString = "World";
+console.log(fristString + secoundString);
+// Problem 5:
+const chal = 153;
+const dal = 261;
+console.log(chal + dal);
+// Problem 6:
+const hasTakaAse = 500;
+const totalKhorosCount = 75;
+const tatalKhorsoKorteParbo = hasTaka / totalUsingCount;
+const obosistoTakathake = hasTakaAse % totalKhorosCount;
+console.log(tatalKhorsoKorteParbo, obosistoTakathake);
+// Problem 7:
+const vagfol = 8 / 3;
+const vagshes = 8 % 3;
+console.log(vagfol, vagshes);
+// Problem 8:
+console.log(50 % 9);
+// Problem 9:
+console.log("Bangla" + "desh");
+// Problem 10:
+const totalChal = 480;
+const parDayChalLage = 4;
+const totalDay = totalChal / parDayChalLage;
+console.log(totalDay);
+const totalMonth = totalDay / 30;
+console.log(totalMonth);
